3891a779aa70f08ce86c8f4bea824d67529aedbe
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-12 12:41:32 -0600

    domain package created. Entities are initially added.

---
+++ Fundamental-Spring-App/src/main/java/com/georgievl/fundamentalspringapp/domain/Instructor.java
@@ -0,0 +1,31 @@
+package com.georgievl.fundamentalspringapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String employeeNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+}
