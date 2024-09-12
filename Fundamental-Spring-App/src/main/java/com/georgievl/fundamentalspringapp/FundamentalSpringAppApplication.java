3982ae3e8cc74117977ce37f2e5b5cc4cfae8248
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-12 11:23:17 -0600

    Initial commit

---
+++ Fundamental-Spring-App/src/main/java/com/georgievl/fundamentalspringapp/FundamentalSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.georgievl.fundamentalspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalSpringAppApplication.class, args);
+    }
+
+}
---
+++ Fundamental-Spring-App/src/test/java/com/georgievl/fundamentalspringapp/FundamentalSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgievl.fundamentalspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
