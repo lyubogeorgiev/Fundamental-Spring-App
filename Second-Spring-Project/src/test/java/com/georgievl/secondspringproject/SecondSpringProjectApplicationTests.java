24a4bd6e5d19e48563e86d44fc4c024877d2d77f
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-12 11:25:51 -0600

    Initial Commit

---
+++ Second-Spring-Project/src/test/java/com/georgievl/secondspringproject/SecondSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgievl.secondspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
