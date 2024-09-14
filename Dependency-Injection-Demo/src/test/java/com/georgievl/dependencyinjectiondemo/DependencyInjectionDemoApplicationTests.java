e5d224386e264faf55b4d78b01ac81de22850dca
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-14 17:46:44 -0600

    Testing the Spring Context

--- Dependency-Injection-Demo/src/test/java/com/georgievl/dependencyinjectiondemo/DependencyInjectionDemoApplicationTests.java
+++ Dependency-Injection-Demo/src/test/java/com/georgievl/dependencyinjectiondemo/DependencyInjectionDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.georgievl.dependencyinjectiondemo;
 
+import com.georgievl.dependencyinjectiondemo.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DependencyInjectionDemoApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+    
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void getControllerFromContext() {
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
+
     @Test
     void contextLoads() {
     }
-
 }
