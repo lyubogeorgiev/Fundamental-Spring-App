80610e31aa68b26010aa16475c497682d4e2ed7c
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-14 17:28:37 -0600

    Spring context bean saved in an object

--- Dependency-Injection-Demo/src/main/java/com/georgievl/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ Dependency-Injection-Demo/src/main/java/com/georgievl/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -1,13 +1,19 @@
 package com.georgievl.dependencyinjectiondemo;
 
+import com.georgievl.dependencyinjectiondemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionDemoApplication.class, args);
-    }
+        ApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
 
+        MyController myController = ctx.getBean(MyController.class);
+
+        System.out.println("I am in main method");
+        System.out.println(myController.sayHello());
+    }
 }
