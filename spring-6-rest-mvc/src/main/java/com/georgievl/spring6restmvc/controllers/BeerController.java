dba87b893f0c7311d19dbe4ae71380c872124a7c
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-15 16:08:24 -0600

    implemented HTTP GET request to return a JSON with a beer by id

--- spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/controllers/BeerController.java
@@ -4,7 +4,9 @@ import com.georgievl.spring6restmvc.model.Beer;
 import com.georgievl.spring6restmvc.sercice.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,16 +15,18 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    public String getBeerById(UUID id) {
+    @RequestMapping(value = "/{beer_id}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beer_id") UUID beer_id) {
         log.debug("Get beer by id was called in the controller.");
-        return beerService.getBeerById(id).toString();
+        return beerService.getBeerById(beer_id);
     }
 }
