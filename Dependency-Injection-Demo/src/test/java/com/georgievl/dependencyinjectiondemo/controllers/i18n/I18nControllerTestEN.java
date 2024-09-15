c5e7d8f9471c0fc32237f69ee64f56bde8a61579
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-15 11:35:48 -0600

    Initial commit

--- Dependency-Injection-Demo/src/test/java/com/georgievl/dependencyinjectiondemo/controllers/i18n/I18nControllerTestEN.java
+++ Dependency-Injection-Demo/src/test/java/com/georgievl/dependencyinjectiondemo/controllers/i18n/I18nControllerTestEN.java
@@ -3,7 +3,6 @@ package com.georgievl.dependencyinjectiondemo.controllers.i18n;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 //@ActiveProfiles("EN")
 @SpringBootTest
