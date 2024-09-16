1911edf4e2ba768e7b6ef79c482734bc3bbd06ba
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-16 11:51:42 -0600

    testing getBeerById into the BeerController using MockMVC

--- spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/controllers/BeerController.java
@@ -2,7 +2,7 @@ package com.georgievl.spring6restmvc.controllers;
 
 import com.georgievl.spring6restmvc.model.Beer;
 import com.georgievl.spring6restmvc.sercice.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
