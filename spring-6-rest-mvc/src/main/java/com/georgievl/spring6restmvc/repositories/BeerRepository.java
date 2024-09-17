5e0ed244cfb1f320bdcdf51de6d149a3b5381e0a
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-17 15:54:07 -0600

    created a Beer repository

---
+++ spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.georgievl.spring6restmvc.repositories;
+
+import com.georgievl.spring6restmvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
