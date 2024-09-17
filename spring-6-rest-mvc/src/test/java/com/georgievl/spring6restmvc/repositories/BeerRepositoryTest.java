f31cc90ff39c7cba5c0689881bf05b7fdcffd602
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-17 17:39:04 -0600

    testing BeerRepository with DataJpaTest

---
+++ spring-6-rest-mvc/src/test/java/com/georgievl/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.georgievl.spring6restmvc.repositories;
+
+import com.georgievl.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer() {
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                        .beerName("New saved beer")
+                .build());
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
+    }
+}
