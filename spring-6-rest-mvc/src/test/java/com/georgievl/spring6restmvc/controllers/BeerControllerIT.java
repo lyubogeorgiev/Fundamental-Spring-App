296fc05cba95ab0ed978c52f6497870ec4459306
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-18 12:19:06 -0600

    implemented beerController Integration Test

---
+++ spring-6-rest-mvc/src/test/java/com/georgievl/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,44 @@
+package com.georgievl.spring6restmvc.controllers;
+
+import com.georgievl.spring6restmvc.model.BeerDTO;
+import com.georgievl.spring6restmvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository repository;
+    @Autowired
+    private BeerRepository beerRepository;
+
+    @Test
+    void testListBeers(){
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
