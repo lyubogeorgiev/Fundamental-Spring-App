6ccc0e244a3c47eac849a1776fb34da9709868d5
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-23 12:30:25 -0600

    implemented validation handler with custom validation body and tested it

---
+++ spring-6-rest-mvc/src/main/java/com/georgievl/spring6restmvc/controllers/CustomErrorController.java
@@ -0,0 +1,27 @@
+package com.georgievl.spring6restmvc.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
+
+        List<Map<String, String>> errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).toList();
+
+        return ResponseEntity.badRequest().body(errorList);
+    }
+}
