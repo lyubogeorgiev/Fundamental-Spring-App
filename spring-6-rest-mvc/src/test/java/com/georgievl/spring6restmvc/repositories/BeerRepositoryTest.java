9cd133c25d0989f62a67b7bc10576062f03ec646
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-09-23 13:48:45 -0600

    JPA validation added and Database Constraint validation added and tested

--- spring-6-rest-mvc/src/test/java/com/georgievl/spring6restmvc/repositories/BeerRepositoryTest.java
+++ spring-6-rest-mvc/src/test/java/com/georgievl/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.georgievl.spring6restmvc.repositories;
 
 import com.georgievl.spring6restmvc.entities.Beer;
+import com.georgievl.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,10 +19,31 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("New saved beer too long of a name, i think already should be over 50 symbols or maybe not")
+                    .beerStyle(BeerStyle.PILSNER)
+                    .upc("1234567")
+                    .price(new BigDecimal("8.76"))
+                    .build());
+
+            beerRepository.flush();
+
+        });
+    }
+
+    @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("New saved beer")
+                        .beerStyle(BeerStyle.PILSNER)
+                        .upc("1234567")
+                        .price(new BigDecimal("8.76"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
